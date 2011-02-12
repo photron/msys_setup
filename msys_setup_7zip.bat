03d81dcf0502c85b586262651c026ac3b0621159
Matthias Bolte <matthias.bolte@googlemail.com>
2011-02-12 15:56:47 +0100

    Update 7zip to version 9.20

--- msys_setup_7zip.bat
+++ msys_setup_7zip.bat
@@ -1,3 +1,3 @@
 if %sentinel% NEQ __sentinel__ exit
 
-call %tmp%\wget_and_install.bat http://downloads.sourceforge.net/sevenzip 7z465.msi %base_dir%\7z
+call %tmp%\wget_and_install.bat http://downloads.sourceforge.net/sevenzip 7z920.msi %base_dir%\7z
